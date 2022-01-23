7356573679cbe93b3b7558620d2982455e5fa427
AndyGuillaume <andy.guillaume75@gmail.com>
2022-01-23 19:01:26 +0100

    set js parser

---
+++ src/js/dig.js
@@ -0,0 +1,132 @@
+const back = (root, road, behind) => {
+    for (let i = 0; i < road.length-behind; i++) {
+        root = isNaN(road[i]) ? root[road[i]] : root[parseInt(road[i], 10)]
+    }
+    return root
+}
+
+const findIdentifier = (iterator, road = []) => {
+    let find = false;
+    for (let property in iterator) {
+        if (iterator[property] === "Identifier" && road[road.length-1] !== "key") return true
+        else if (iterator[property] !== null && typeof(iterator[property]) === "object") {
+            find = findIdentifier(iterator[property], [...road, property])
+            if (find) return true
+        }
+    }
+    return false
+}
+
+const hasIdentifier = (init) => {
+    let iterator;
+    if (init['type'] === "Literal") return false
+    else if (init['type'] === "ArrayExpression" || init['type'] === "ObjectExpression") {
+        iterator = init['type'] === "ArrayExpression" ? init['elements'] : init['properties']
+        return findIdentifier(iterator)
+    }
+    return true
+}
+
+const dig_function = (root, tree, road = [], list_function = []) => {
+    let name, range_full, sub_tree, params, range_params, range_body, async;
+    for (let branch in tree) {
+        if (branch === "type" && tree[branch] === "VariableDeclarator" && tree['init']) {
+            if (tree['init']['type'] === "ArrowFunctionExpression" || tree['init']['type'] === "FunctionExpression") {
+                sub_tree = back(root, road, 2)
+                async = tree['init']['async']
+                name = tree['id']['name']
+                params = tree['init']['params']
+                if (params.length > 0) {
+                    range_params = tree['init']['params'].length-1 === 0 
+                    ? tree['init']['params'][0]['range']
+                    : [tree['init']['params'][0]['range'][0], tree['init']['params'][tree['init']['params'].length-1]['range'][1]]
+                } else range_params = null
+                range_full = sub_tree['range']
+                range_body = tree['init']['body']['range']
+                range_body[0]++; range_body[1]--;
+                list_function.push({ async, name, params, range_params, range_body, range_full })
+            }
+        }
+		else if (branch === "type" && tree[branch] === "FunctionDeclaration") {
+            async = tree['async']
+			name = tree['id']['name']
+            params = tree['params']
+            range_full = tree['range']
+            range_body = tree['body']['range']
+            range_body[0]++; range_body[1]--;
+            list_function.push({ async, name, params, range_params, range_body, range_full })
+		}
+        else if (tree[branch] !== null && typeof(tree[branch]) === "object") {
+            dig_function(root, tree[branch], [...road, branch], list_function)
+		}
+    }
+    return list_function
+}
+
+const dig_variable = (root, tree, road = [], list_variable = []) => {
+    let name, range_full, sub_tree, range_body, init_type, range_name;
+    for (let branch in tree) {
+        if (branch === "type" && tree[branch] === "VariableDeclarator") {
+            sub_tree = back(root, road, 2)
+            name = tree['id']['name']
+            range_name = tree['id']['range']
+            range_full = sub_tree['range']
+            range_body = tree['init'] ? tree['init']['range'] : null;
+            if (tree['init']) init_type = hasIdentifier(tree['init']) ? "Identifier" : "Literal"
+            else init_type = "None"
+            list_variable.push({ name, range_body, range_full, range_name, init_type })
+        }
+        else if (tree[branch] !== null && typeof(tree[branch]) === "object") {
+            sub_tree = back(root, road, 1)
+            if (!(branch === "body" && tree['type'] === "FunctionExpression" && !sub_tree['callee'])) {
+                dig_variable(root, tree[branch], [...road, branch], list_variable)
+            }
+		}
+    }
+    return list_variable
+}
+
+const get_call = (root, tree, list_variable, list_function, local_var = [], road = [], list_call = []) => {
+    let sub_tree;
+    for (let branch in tree) {
+        if (branch === "type" && tree[branch] === "VariableDeclarator") {
+            local_var.push(tree['id']['name'])
+        }
+        else if (branch === "type" && tree[branch] === "Identifier") {
+			if (!local_var.includes(tree['name']) && (list_variable.includes(tree['name']) || list_function.includes(tree['name']))) {
+                sub_tree = back(root, road, 2)
+                if (!sub_tree['params']) list_call.push({ name: tree['name'], range: tree['range']})
+            }
+		}
+        else if (tree[branch] !== null && typeof(tree[branch]) === "object") {
+            get_call(root, tree[branch], list_variable, list_function, local_var, [...road, branch], list_call)
+		}
+    }
+    return list_call
+}
+
+const get_self_exec = (root, tree, road = [], list_self_exec = []) => {
+    let sub_tree;
+    let range_full, range_body, params;
+    for (let branch in tree) {
+        if (branch === "type" && tree[branch] === "FunctionExpression") {
+            sub_tree = back(root, road, 1)
+            if (sub_tree['type'] === "CallExpression") {
+                range_full = sub_tree['range']
+                range_body = tree['body']['range']
+                range_body[0]++; range_body[1]--
+                params = []
+                tree['params'].forEach((p, i) => {
+                    params.push({ name: p.name, range_value: sub_tree['arguments'][i].range })
+                });
+                list_self_exec.push({ range_full, range_body, params })
+            }
+        }
+        else if (tree[branch] !== null && typeof(tree[branch]) === "object") {
+            list_self_exec = get_self_exec(root, tree[branch], [...road, branch], list_self_exec)
+		}
+    }
+    return list_self_exec
+}
+
+export {dig_function, dig_variable, get_call, get_self_exec}
---
+++ src/js/parser.js
@@ -0,0 +1,165 @@
+import esprima from "esprima"
+import {dig_function, dig_variable, get_call, get_self_exec} from "./dig.js"
+import test from "./test.js"
+import beautify from 'js-beautify'
+
+const THIS = "self."
+
+const removecomments = (string) => string.replace(/\/\*[\s\S]*?\*\/|\/\/.*/g,'').trim();
+
+const replacebyspace = (string, range) => string.slice(0, range[0]) + Array(range[1] - range[0]).fill(" ").join("") + string.slice(range[1])
+
+const addcode = (string, range, code) => string.slice(0, range[0]) + code + string.slice(range[0])
+
+const getcontentofrange = (string, range) => string.slice(range[0], range[1])
+
+const objtoarray = (obj, property) => obj.map(o => o[property])
+
+const getparameters = (obj, property) => objtoarray(obj.map(o => o['name'] ? o : o['left']), property)
+
+const cleanfunction = (func) => getcontentofrange(func, [func.indexOf('{')+1, func.length-1])
+ 
+
+const eqArray = (a,b) => {
+	if (a.length !== b.length) return false
+	for (let i = 0; i < a.length; i++) {
+		if (a[i] !== b[i]) return false
+	}
+	return true
+}
+
+const parse_js_file = (prog) => {
+    let i, j;
+    let list_variable, list_function, list_call, list_calls, list_self_exec;
+    let tree, original, duplicate;
+    let data = [], methods = [], created;
+
+    original = prog = removecomments(prog)
+
+    tree = esprima.parse(prog, {range: true})
+    list_function = dig_function(tree, tree)
+    list_function.forEach(f => {
+        f.code = getcontentofrange(original, f.range_full)
+        prog = replacebyspace(prog, f.range_full)
+    })
+
+    tree = esprima.parse(prog, {range: true})
+    list_variable = dig_variable(tree, tree)
+
+    list_calls = []
+    list_function.forEach(f => {
+        tree = esprima.parse(f.code, {range: true})
+        list_call = get_call(tree, tree, objtoarray(list_variable, 'name'), objtoarray(list_function, 'name'), getparameters(f.params, 'name'))
+        list_calls = [...list_calls, ...list_call]
+        for (let call of list_call.reverse()) {
+            f.code = addcode(f.code, call.range, THIS)
+        }
+        f.code = cleanfunction(f.code)
+        f.code = `const ${THIS} = this;${f.code}` 
+        methods.push({ name: f.name, async: f.async, params: f.params.length ? getcontentofrange(original, f.range_params) : "", code: f.code })
+    })
+    
+
+    list_variable.reverse().forEach((v, i) => {
+        if (objtoarray(list_calls, 'name').includes(v.name)) {
+            if (v.init_type === "None" || v.init_type === "Literal" ) {
+                prog = replacebyspace(prog, v.range_full)
+                data.push({ name: v.name, value: v.body })
+            }
+            else {
+                prog = replacebyspace(prog, [v.range_full[0], v.range_name[0]])
+                prog = addcode(prog, v.range_name, THIS)
+                v.range_name[0] += THIS.length * (list_variable.length-i); 
+                v.range_name[1] += THIS.length * (list_variable.length-i);
+                data.push({ name: v.name, value: "" })
+            }
+        }
+    })
+
+    tree = esprima.parse(prog, {range: true})
+    list_call = get_call(tree, tree, objtoarray(list_variable, 'name'), objtoarray(list_function, 'name'))
+
+    for (i = 0; i < list_call.length; i++) {
+        for (j = 0; j < list_variable.length; j++) {
+            if (eqArray(list_variable[j]['range_name'], list_call[i]['range'])) {
+                list_call.splice(i--, 1)
+                break
+            }
+        }
+    }
+    
+    list_call.reverse().forEach(c => {
+        if (objtoarray(list_variable, 'name').includes(c['name']) || objtoarray(list_function, 'name').includes(c['name'])) {
+            prog = addcode(prog, c['range'], THIS)
+        }
+    })
+
+    tree = esprima.parse(prog, {range: true})
+    list_self_exec = get_self_exec(tree, tree)
+
+    duplicate = prog
+
+    if (list_self_exec.length) {
+        prog = getcontentofrange(prog, list_self_exec[0].range_body)
+        list_self_exec[0].params.forEach(p => {
+            prog = `let ${p.name} = ${getcontentofrange(duplicate, p.range_value)};\n` + prog
+        })
+    }
+
+    created = `const self = this; ${prog}`
+
+    //console.log(created)
+    
+    return {
+        data,
+        methods,
+        created
+    }
+}
+
+const vanilla_to_vue = (info) => {
+    let created = "", methods = "", data = ""
+    info['data'].forEach(v => {
+        data += `${v.name}: ${v.value ? v.value : "\"\""},\n`
+    })
+    data = `data() {\nreturn {\n${data}}\n}`
+    info['methods'].forEach(f => {
+        methods += `${f.name}(${f.params}){${f.code}}\n`
+    })
+    methods = `methods: {\n${methods}\n}`
+    created = `created() {${info['created']}}`
+    return beautify.js(`export default {
+        name: 'xxx-name',
+        props: {},
+        ${created},
+        ${data},
+        ${methods}
+    }`)
+}
+
+const program = `
+(function() {
+    let x = z;
+    const w = x;
+    const a = (y = 3) => {x = w}
+    const b = function() {
+        let v = x;
+        v = w;
+        return 3;
+    }
+    function c() {
+        let v = x;
+        let y = a()
+        b()
+    }
+    c()
+    x.addEventlistener("event", function(){
+        let t = x
+        a()
+    })
+})()
+`
+
+
+//parse_js_file(test)
+console.log(vanilla_to_vue(parse_js_file(test)))
